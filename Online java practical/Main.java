179b1fee73305406c8a223d9489d7242cf9584b8
ankur-mukherjee2003 <120292298+ankur-jod@users.noreply.github.com>
2024-05-26 08:18:47 +0530

    Update Main.java

--- Online java practical/Main.java
+++ Online java practical/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         String str = "system";
 
-        System.out.println(str.indexOf("t")); // Outputs: 2
-        System.out.println(str.indexOf("e")); // Outputs: 1
-        System.out.println(str.indexOf("s")); // Outputs: 0
-        System.out.println(str.indexOf("y")); // Outputs: 1
+        System.out.println(str.indexOf("t"));     // Outputs: 3
+        System.out.println(str.indexOf("e"));    //  Outputs: 4
+        System.out.println(str.indexOf("s"));   //   Outputs: 0
+        System.out.println(str.indexOf("y"));  //    Outputs: 1
     }
 }
